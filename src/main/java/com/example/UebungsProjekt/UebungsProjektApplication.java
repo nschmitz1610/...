8634316f27c4e1343afb6ed640fcd8322b6fbf38
nschmitz1610 <nathalie-schmitz@hotmail.com>
2021-04-15 11:15:21 +0200

    Initial Commit

---
+++ src/main/java/com/example/UebungsProjekt/UebungsProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.UebungsProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungsProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UebungsProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UebungsProjekt/UebungsProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UebungsProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungsProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
